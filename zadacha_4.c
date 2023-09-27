d0a8c57b233ad08e7193dda388dca546173bbbd7
VVorsTeammate <145848526+VVorsTeammate@users.noreply.github.com>
2023-09-27 21:10:49 +0300

    Update zadacha_4.c

--- zadacha_4.c
+++ zadacha_4.c
@@ -3,6 +3,7 @@
 int main(void){
     int V_1, V_2, S, T, S_t; // V_1 - скорость первого авто; V_2 - скорость второго авто
                         // S- расстояние между 1 и 2 авто; Т - время движения; S_t - расстояние через Т часов
+    printf("Enter the speed of the first car, the speed of the second, the distance between them and the driving time\n");
     scanf("%d %d %d %d", &V_1, &V_2, &S, &T);
 
     if (0 > V_1 || V_2 < 0 || T < 0 || S < 0){
