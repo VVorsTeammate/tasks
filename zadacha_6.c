66a07134eccc909b47640c478f7e9e9c1235cbd5
VVorsTeammate <145848526+VVorsTeammate@users.noreply.github.com>
2023-09-27 21:11:48 +0300

    Update zadacha_6.c

--- zadacha_6.c
+++ zadacha_6.c
@@ -7,9 +7,12 @@ int main(void){
     scanf("%d %d", &x_1, &y_1);
     scanf("%d %d", &x_2, &y_2);
     scanf("%d %d", &x_3, &y_3);
-
+    
+    printf("Enter the coordinates of vertex A\n");
     AB = sqrt(pow((x_2 - x_1),2) + pow((y_2 - y_1), 2));
+    printf("Enter the coordinates of vertex B\n");
     BC = sqrt(pow((x_3 - x_2),2) + pow((y_3 - y_2), 2));
+    printf("Enter the coordinates of vertex C\n");
     AC = sqrt(pow((x_1 - x_3),2) + pow((y_1 - y_3), 2));
 
     P = AB + BC + AC;
